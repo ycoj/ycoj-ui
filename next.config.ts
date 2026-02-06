6371b267f0489b2ab43edcc405003e472166ea9b
Tairui Qu <XiaoQuQu_CN@outlook.com>
2026-02-06 15:11:53 +0800

    fix: format

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   env: {
     SITE_NAME: process.env.SITE_NAME ?? '',
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://next-cdn.ycoj.cc' : '',
+  assetPrefix:
+    process.env.NODE_ENV === 'production' ? 'https://next-cdn.ycoj.cc' : '',
   async rewrites() {
     return [
       {
