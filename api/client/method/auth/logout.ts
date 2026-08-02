b57586de2a75789bd910f834ba40e5b3abc7ea8a
Tairui Qu <XiaoQuQu_CN@outlook.com>
2026-08-02 11:49:56 +0800

    fix: add timeout

--- api/client/method/auth/logout.ts
+++ api/client/method/auth/logout.ts
@@ -3,5 +3,5 @@ import { clientRequest } from '@/api/client';
 export type LogoutResponse = {
   url: string;
 };
-
-export const logout = () => clientRequest.Post<LogoutResponse>('/logout', {});
+export const logout = () =>
+  clientRequest.Post<LogoutResponse>('/logout', {}, { timeout: 10_000 });
